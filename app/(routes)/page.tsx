2cbfa27d77dd29d2908579b646643aca0623ec8d
ajay-mandal <ajayrox48@gmail.com>
2024-10-16 21:31:48 +0530

    billboard page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,19 @@
-import Container from '@/components/ui/container'
 import React from 'react'
 
-const HomePage = () => {
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container'
+
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+    const billboard = await getBillboard('id');
   return (
     <Container>
         <div className="space-y-10 pb-10">
-
+            <Billboard data={billboard}/>
         </div>
     </Container>
   )
