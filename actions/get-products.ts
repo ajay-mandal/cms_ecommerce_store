b6fc9dc3157ef04e454b1c9f8bfc30f0c8b33a6e
ajay-mandal <ajayrox48@gmail.com>
2024-10-19 21:30:14 +0530

    product ..

--- actions/get-products.ts
+++ actions/get-products.ts
@@ -13,7 +13,7 @@ interface Query {
 
 const getProducts = async (query: Query) : Promise<Product[]> => {
 
-    const url = qs.stringify({
+    const url = qs.stringifyUrl({
         url: URL,
         query: {
             colorId: query.colorId,
@@ -23,7 +23,7 @@ const getProducts = async (query: Query) : Promise<Product[]> => {
         }
     });
 
-    const res = await fetch(URL);
+    const res = await fetch(url);
 
     return res.json();
 }
